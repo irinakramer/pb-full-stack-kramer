dc05a9a9bb3f796005130c95c9856cf0bcaa80ce
Irina Kramer <irakramer@gmail.com>
2023-05-28 09:26:04 -0700

    added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as sighting_router
@@ -6,6 +7,14 @@ from routes import router as sighting_router
 config = dotenv_values(".env.local")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def startup_db_client():
