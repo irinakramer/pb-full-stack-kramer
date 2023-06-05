6c73d35adf9d7229b1fc27a8803202a62a30a266
Irina Kramer <irakramer@gmail.com>
2023-06-05 10:24:55 -0700

    deploy files

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as sighting_router
+import uvicorn
 
 config = dotenv_values(".env.local")
 
@@ -27,3 +28,6 @@ def shutdown_db_client():
     app.mongodb_client.close()
 
 app.include_router(sighting_router, tags=["sightings"], prefix="/sighting")
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
